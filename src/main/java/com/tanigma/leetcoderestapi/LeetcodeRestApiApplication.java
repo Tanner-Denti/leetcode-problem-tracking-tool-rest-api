f766d555789039b525e124b970582e5e7f0c4ceb
Tanner Denti <tannerwdenti@gmail.com>
2024-05-08 20:35:40 -0500

    Initialize base project

---
+++ src/main/java/com/tanigma/leetcoderestapi/LeetcodeRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.tanigma.leetcoderestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeRestApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tanigma/leetcoderestapi/LeetcodeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanigma.leetcoderestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
